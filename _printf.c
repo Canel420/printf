c0bf726bc842c23500aad202cfa133a23b7bf067
Canel420 <4521@holbertonschool.com>
2022-03-22 16:16:45 -0500

    printf function

---
+++ _printf.c
@@ -0,0 +1,87 @@
+#include "main.h"
+
+/**
+ * convers - Check if format elements is an specifier.
+ *
+ * @fmt: Format element
+ *
+ * Description: Search if the formant element is an specifier for
+ * conversion and calls the corresponding conversion function.
+ *
+ * Return: Pointer to conversion function.
+ */
+
+int (*convers(const char *fmt))(va_list)
+{
+	temp check[] = {
+		{"c", p_char},
+		{"s", p_str},
+		{"i", p_int},
+		{"d", p_dec},
+		{"r", p_str_r},
+		{NULL, NULL}
+	};
+
+	int i;
+
+	for (i = 0 ; check[i].s; i++)
+	{
+		if (*fmt == *(check[i].s))
+			break;
+	}
+	return (check[i].f);
+}
+
+/**
+ * _printf - Print a string that can be modified with specifiers
+ * to include any type of data.
+ *
+ * @format: String to print
+ *
+ * Description: Tages a string given and replace
+ * specifiers with any type of data requiered.
+ *
+ * Return: The amount of characters printed.
+ */
+
+int _printf(const char *format, ...)
+{
+	int i, len = 0;
+	va_list args;
+	int (*f)(va_list);
+
+	if (format == NULL)
+		return (-1);
+
+	va_start(args, format);
+
+	for (i = 0; format[i]; i++)
+	{
+		if (format[i] == '\'' && format[i + 1] == 'n')
+			_putchar('\n');
+
+		if (format[i] == '%')
+		{
+			if (format[i + 1] == '%')
+			{
+				_putchar(format[i + 1]);
+				i++;
+			}
+			if (!format[i + 1])
+				return (-1);
+			f = convers(&format[i + 1]);
+			if (f != NULL)
+			{
+				len += f(args);
+				i++;
+				continue;
+			}
+		}
+		else
+		{
+			_putchar(format[i]);
+			len++; }
+	}
+	va_end(args);
+	return (len);
+}
