d28ae355454d1d98d0a3b7a722890db2cb514de0
Canel420 <4521@holbertonschool.com>
2022-03-22 20:09:57 -0500

    add '\o' case

--- _printf.c
+++ _printf.c
@@ -67,12 +67,6 @@ int _printf(const char *format, ...)
 				i++;
 				len++;
 			}
-			if (format[i + 1] == '\0')
-			{
-				_putchar(format[i]);
-				len++;
-				return (len);
-			}
 			if (!format[i + 1])
 				return (-1);
 			f = convers(&format[i + 1]);
