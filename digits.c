0adfe1d0d7860c7f97ff6ce8681eb9ad30d0bc67
Canel420 <4521@holbertonschool.com>
2022-03-22 20:45:44 -0500

    Trying optional code

--- digits.c
+++ digits.c
@@ -56,38 +56,37 @@ int p_dec(va_list d)
 
 int p_int(va_list i)
 {
-	int count, n2, num2;
-	int n, num3, div;
+	int arr[10];
+	int n, m, j, sum, len = 0;
 
 	n = va_arg(i, int);
-	n2 = n;
-	count = 0;
+	m = 1000000000;
+	arr[0] = n / m;
 
-	while (n2 != 0)
+	for (j = 1; j < 10; j++)
 	{
-		n2 = n2 / 10;
-		count++;
+		m /= 10;
+		arr[j] = (n / m) % 10;
+	}
+	if (n < 0)
+	{
+		_putchar('-');
+		len++;
+		for (j = 0; j < 10 ; j++)
+			arr[j] *= -1;
 	}
 
-	div = 1;
-	for (num2 = 1 ; num2 < count ; num2++)
+	for (j = 0, sum = 0; j < 10; j++)
 	{
-		div = div * 10;
-	}
-	if (n == 0)
-	{
-		_putchar('0');
-		return (1);
-	}
-	else if (n > 0)
-	{
-		for (num3 = count + 1 ; num3 > 1 ; num3--)
+		sum += arr[j];
+		if (sum != 0 || j == 9)
 		{
-			_putchar ((n / div) % 10 + '0');
-			div = div / 10;
+			_putchar('0' + arr[j]);
+			len++;
 		}
 	}
-	return (count);
+
+	return (len);
 }
 /**
  * p_int_b - Convert an unsigned number to binary
